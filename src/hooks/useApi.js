ce65706fe509f140f4f2c65e287fd8295c8d814a
khinsuwai97 <khinsuwai009@gmail.com>
2023-02-06 19:23:26 +0630

    added localStorage

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,36 +1,36 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import axios from 'axios';
 
 export const API_KEY = 'f7f4fb6602b14a4794ac47f148e5b48b';
 
 const useApi = (storageName) => {
-  const [meal, setMeal] = useState(
-    JSON.parse(localStorage.getItem(storageName)) || []
-  );
+  const [meal, setMeal] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const fetchMeal = useCallback(async (value) => {
-    if (meal) {
-      return;
-    }
-    setLoading(true);
-    try {
-      const { data } = await axios.get(
-        `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=9&${
-          value ? `tags=${value}` : ''
-        }`
-      );
+    const check = localStorage.getItem(storageName);
+    if (check) {
+      setMeal(JSON.parse(check));
+    } else {
+      setLoading(true);
+      try {
+        const { data } = await axios.get(
+          `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=9&${
+            value ? `tags=${value}` : ''
+          }`
+        );
 
-      setMeal(data.recipes);
-      setLoading(false);
-    } catch (err) {
-      if (err) {
-        console.error(err);
-        setError('Error with fetching meal');
+        setMeal(data.recipes);
+        setLoading(false);
+      } catch (err) {
+        if (err) {
+          console.error(err);
+          setError('Error with fetching meal');
+        }
       }
+      setLoading(false);
     }
-    setLoading(false);
   }, []);
 
   return { meal, error, loading, fetchMeal };
